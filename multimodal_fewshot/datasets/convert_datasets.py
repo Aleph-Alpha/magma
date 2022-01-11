8fc542c714f01284545147114ae027f57bca9aca
constantin_eichenberg <constantin.eichenberg@mailbox.org>
2022-01-11 19:01:58 +0100

    refactor convert_datasets.py

--- multimodal_fewshot/datasets/convert_datasets.py
+++ multimodal_fewshot/datasets/convert_datasets.py
@@ -5,11 +5,6 @@ import json
 from pathlib import Path
 from tqdm import tqdm
 from imagehash import phash
-
-try:
-    from .dataset import get_dataset
-except ImportError:
-    from dataset import get_dataset
 import shutil
 
 
@@ -36,89 +31,15 @@ def save_images(img_list, target_dir, mode="mv"):
             shutil.copy(img_path, target_dir)
 
 
-def parse_dataset(dataset_type, dataset=None):
-    """
-    should return an iterator yielding an image path (as Path object) and corresponding image data in a dictionary
-
-    image_path, {"captions": [...], "metadata": {...}, }
-    """
-    img_data_list = []
-
-    print(f"parsing {dataset_type} dataset")
-
-    if dataset_type == "hateful_memes":
-        for img, caption, _ in dataset.imgs_txt_labels:
-            img_path = Path(f"{dataset.main_dir}/{img}")
-            img_data = {
-                "captions": [caption],
-                "metadata": {"dataset": f"{dataset_type}"},
-            }
-            img_data_list.append((img_path, img_data))
-
-    if "vqa" in dataset_type:
-        if "train" in dataset_type:
-            img_prefix = "COCO_train2014_"
-        elif "val" in dataset_type:
-            img_prefix = "COCO_val2014_"
-        for q_a in dataset.q_a_list:
-            img_path = Path(f'{dataset.img_dir}/{img_prefix}{q_a["image_id"]:012}.jpg')
-
-            captions = [
-                f'Q: {q_a["question"]} A: {a["answer"]}' for a in q_a["answers"]
-            ]
-            img_data = {
-                "captions": captions,
-                "metadata": {"dataset": f"{dataset_type}", "q_a_dict": q_a},
-            }
-            img_data_list.append((img_path, img_data))
-
-    if "gqa" in dataset_type:
-        for q, a, img_path in iter(dataset):
-            caption = f"Q: {q} A: {a}"
-            img_data = {
-                "captions": [caption],
-                "metadata": {"dataset": f"{dataset_type}", "question": q, "answer": a},
-            }
-            img_data_list.append((Path(img_path), img_data))
-
-    if dataset_type == "coco":
-        for idx in dataset.ids:
-            ann_ids = dataset.coco.getAnnIds(imgIds=idx)
-            anns = dataset.coco.loadAnns(ann_ids)
-            filename = dataset.coco.imgs[idx]["file_name"]
-            img_path = Path(f"{dataset.root}/{filename}")
-            captions = [ann["caption"] for ann in anns]
-            img_data = {
-                "captions": captions,
-                "metadata": {"dataset": f"{dataset_type}"},
-            }
-            img_data_list.append((img_path, img_data))
-
-    return img_data_list
-
-
 def convert_dataset(
-    data_dir,
-    dataset_type=None,
-    dir_size=10000,
-    hash_fn=None,
-    mode="mv",
-    delete_temp=True,
-    ds_iterator=None,
-    split="train",
+    data_dir, dir_size=10000, hash_fn=None, mode="mv", ds_iterator=None,
 ):
+    """
+    Builds a dataset directory in our standard format. ds_iterator should return data of the form
+    image_path, {"captions": [...], "metadata": {...}, }, where image_path should be a Path object.
+    """
 
-    assert mode in ["mv", "cp"]
     data_dir = Path(data_dir)
-
-    # temporary directory to download and store the dataset
-    if ds_iterator is None:
-        temp_dir = data_dir / "temp"
-        os.makedirs(temp_dir, exist_ok=True)
-        ds = get_dataset(dataset_type, temp_dir, mode=split)
-        ds_iterator = parse_dataset(dataset_type, dataset=ds)
-    else:
-        temp_dir = None
 
     # folders for images and corresponding data which is stored in a json file for each image
     os.makedirs(data_dir / "images", exist_ok=True)
@@ -134,8 +55,7 @@ def convert_dataset(
     new_img_locations = {}
 
     pbar = tqdm(
-        enumerate(ds_iterator),
-        desc="converting dataset to standard format...",
+        enumerate(ds_iterator), desc="converting dataset to standard format...",
     )
 
     for k, (img_path, data) in pbar:
@@ -192,24 +112,3 @@ def convert_dataset(
             img_data_list = []
             save_data_dir = data_dir / "image_data" / f"{int((k+1)/dir_size)}/"
 
-    # delete temp folder when done
-    if delete_temp and temp_dir is not None:
-        os.system(f"rm -r {str(temp_dir)}")
-
-
-class DatasetEditor:
-    def __init__(self, edit_fns):
-        self.edit_fns = edit_fns
-
-    def __call__(self, data_dir):
-        data_dir = Path(data_dir)
-        data_list = data_dir.rglob("*.json")
-        pbar = tqdm(data_list, desc="Editing dataset...")
-        for data in pbar:
-            for fn in self.edit_fns:
-                fn(data)
-
-
-if __name__ == "__main__":
-
-    print("done")
